ab8d7ef24773947a4da0b9ad93a0de2457931d64
C04H03 <C04H03@teb.edu.pl>
2024-04-23 14:46:25 +0200

    KONIEC

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,50 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        Scanner scanner = new Scanner(System.in);
-        Set<String> setZImionami = new HashSet<>();
+//        Scanner scanner = new Scanner(System.in);
+//        Set<String> setZImionami = new HashSet<>();
+//
+//        while(true) {
+//            System.out.println("Dodaj imie do zbioru(Wprowadz '-' jezeli chcesz zakonczyc): ");
+//            String input = scanner.nextLine();
+//
+//            if(input.equals("-")) {
+//                break;
+//            }
+//            setZImionami.add(input);
+//        }
+//
+//        System.out.println(setZImionami);
+//        System.out.println(setZImionami.size());
+
+        Scanner input = new Scanner(System.in);
+        Map<String, String> pary = new HashMap<>();
+        String imie1, imie2;
 
         while(true) {
-            System.out.println("Dodaj imie do zbioru(Wprowadz '-' jezeli chcesz zakonczyc): ");
-            String input = scanner.nextLine();
-
-            if(input.equals("-")) {
+            System.out.println("Podaj imie partnera: ");
+            imie1 = input.next();
+            if (imie1.equals("-")) {
                 break;
             }
-            setZImionami.add(input);
-        }
+            System.out.println("Podaj imie partnerki: ");
+            imie2 = input.next();
+            if (imie2.equals("-")) {
+                break;
+            }
+            pary.put(imie1, imie2);
 
-        System.out.println(setZImionami);
-        System.out.println(setZImionami.size());
+        }
+        System.out.println("Podaj imie partnera do sprawdzenia: ");
+        imie1 = input.next();
+        System.out.println("Partnerka dla " + imie1 + " to " + pary.get(imie1));
+
+
+
+
 
     }
 }
